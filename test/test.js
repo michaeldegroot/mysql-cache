091601be2a9b2e993dc31f332fc148d3e0055ad7
michaeldegroot <specamps@gmail.com>
2015-12-09 08:58:48 +0100

    travis what do you think of this lovely mocha test

--- test/test.js
+++ test/test.js
@@ -20,6 +20,12 @@ describe("Internal", function(){
 	it('Show stats', function(){
 		db.stats();
 	});
+	it('Call a query', function(done){
+		db.query("SELECT ? + ? AS solution",[1,5],function(resultMysql){ // the SQL contains a SELECT which means it will be cached for future use.
+			assert.equal(resultMysql[0].solution,6);
+			done();
+		});
+	});
 	
 	it('Change DB', function(){
 		db.changeDB({user:"testusername",pass:"keepo",database:"kappa",charset:"utf8"}, function(err){  // Change database connection settings on the fly.
@@ -27,19 +33,6 @@ describe("Internal", function(){
 				if(err) throw err;
 			}, Error);
 		})
-	});
-	it('Call a query', function(done){
-		db.query("SELECT ? + ? AS solution",[1,5],function(resultMysql){ // the SQL contains a SELECT which means it will be cached for future use.
-			db.query("SELECT ? + ? AS solution",[1,5],function(resultCached){ // This exact SQL has been executed before and will be retrieved from cache.
-				db.delKey("SELECT ? + ? AS solution",[1,5]); // Delete this SQL cache key.
-				db.query("SELECT ? + ? AS solution",[1,5],function(resultRemoved){ // This SQL will be executed on the database because the sql cache key was deleted.
-					console.log("Result from mysql is: "+resultMysql[0].solution);
-					console.log("Result cached is: "+resultCached[0].solution);
-					console.log("Result after cache key is deleted: "+resultRemoved[0].solution);
-					done();
-				});
-			},{cache:false}); // Do not cache this query.
-		},{TTL:600}); // Set TTL to 600 only for this query.
 	});
 });
 return;

cb736fd9bede1e0911a4796dd0b381f21a4fda63
Michael de Groot <specamps@gmail.com>
2017-01-22 04:08:00 +0100

    test travis env var

--- test/settings.js
+++ test/settings.js
@@ -1,13 +1,22 @@
 exports.settings = () => {
-    return {
-        host: '127.0.0.1',
-        user: 'root',
-        password: '',
-        database: 'mysqlcache',
-        TTL: 0,
+    const sets = {
+        host:            '127.0.0.1',
+        user:            'root',
+        password:        'developer',
+        database:        'mysqlcache',
+        TTL:             0,
         connectionLimit: 10000,
-        verbose: true,
-        caching: true,
-        cacheProvider: 'node-cache'
+        verbose:         true,
+        caching:         true,
+        cacheProvider:   'node-cache'
     }
+
+    if (process.env.hasOwnProperty('CI') === true) {
+        if (process.env.CI) {
+            sets.user     = 'root'
+            sets.password = ''
+        }
+    }
+
+    return sets
 }
